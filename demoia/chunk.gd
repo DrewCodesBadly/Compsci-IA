bf6959f3e5d7266be34bb8b028826d23d6271544
Dhruva K <dhruvak@outlook.com>
2025-02-11 15:08:18 -0500

    godot objects for creating a scene

---
+++ demoia/chunk.gd
@@ -0,0 +1,13 @@
+@tool
+
+extends Node2D
+
+@export var floor: Sprite2D
+@export var u_wall: Line2D
+@export var l_wall: Line2D
+@export var d_wall: Line2D
+@export var r_wall: Line2D
+
+# ADD
+func init_from_chunk() -> void:
+	pass
--- demoia/main.gd
+++ demoia/main.gd
@@ -4,7 +4,7 @@ extends Node
 @export var generator: TerrainGenerator
 @export var gen_toggle: bool = false:
 	set(v):
-		generator.generate()
+		generate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +15,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+func generate() -> void:
+	generator.generate()
